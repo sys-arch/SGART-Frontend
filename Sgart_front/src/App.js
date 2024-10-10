04f066f454d1a25144919795257bd2e7f0a1706b
Alejandro Galán Moreno <alejandro.galan@alu.uclm.es>
2024-10-10 12:01:51 +0200

    Actualización css (ya sin conflictos)

--- Sgart_front/src/App.js
+++ Sgart_front/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
+import GoogleAuth from './components/GoogleAuth';
 
 function App() {
   return (
       <div className="App">
-          
+          <GoogleAuth />
           <RegisterForm />
       </div>
   );
