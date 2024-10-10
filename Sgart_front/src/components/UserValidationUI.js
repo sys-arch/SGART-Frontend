1b03916db714ab992cf24e44473bb2100c39f8bf
Manuel <manuel.perales@alu.uclm.es>
2024-10-10 16:03:12 +0200

    Interfaz de validar usuarios terminada

--- Sgart_front/src/components/UserValidationUI.js
+++ Sgart_front/src/components/UserValidationUI.js
@@ -2,18 +2,36 @@ import React, { useState } from 'react';
 
 const UserValidationUI = () => {
 
-    // Estados de cada campo del formulario
-    const [email_textbox, setEmail] = useState('');
-    const [contrasena_textbox, setContrasena] = useState('');
-    const [repetirContrasena_textbox, setRepetirContrasena] = useState('');
-    const [error, setError] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-
     const handleSubmit = (event) => {
         event.preventDefault();
-
         
     }
+    // Estado que contiene los datos de la tabla
+    const [datosUsuarios, setDatosUsuarios] = useState([
+        { id: 1, Nombre: 'Juan', Apellidos: 'Pérez',Email: 'juan.perez@example.com'},
+        { id: 2, Nombre: 'María', Apellidos: 'López', Email: 'maria.lopez@example.com'},
+        { id: 3, Nombre: 'Carlos', Apellidos: 'García', Email: 'carlos.garcia@example.com'},
+    ]);
+
+    var nextUserId=datosUsuarios.length;
+
+    const [datosValidar, setDatosValidar] = useState([
+        { id: 1, Nombre: 'Manuel', Apellidos: 'Perales',Email: 'manuel.perales@example.com'},
+    ]);
+
+    const invalidarUsuario = (id) => {
+        // Filtramos el array de datos para eliminar el elemento con el id correspondiente
+        const nuevosDatos = datosValidar.filter((item) => item.id !== id);
+        setDatosValidar(nuevosDatos); // Actualizamos el estado con los nuevos datos
+    };
+
+    const validarUsuario = (id) => {
+        var usuario = datosValidar.filter((item) => item.id === id);
+        const nuevosDatos = datosValidar.filter((item) => item.id !== id);
+        setDatosValidar(nuevosDatos); // Actualizamos el estado con los nuevos datos
+        setDatosUsuarios(datosUsuarios.concat(usuario)); // Actualizamos el estado con los nuevos datos
+    }
+
     return (
         <div class="user-validation-container">
         <div class="admin-buttons">
@@ -33,6 +51,7 @@ const UserValidationUI = () => {
                 <table class="user-table">
                     <thead>
                         <tr>
+                            <th>id</th>
                             <th>Nombre</th>
                             <th>Apellidos</th>
                             <th>Email</th>
@@ -40,15 +59,18 @@ const UserValidationUI = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Manuel</td>
-                            <td>Perales</td>
-                            <td>manuel.perales@example.com</td>
+                    {datosValidar.map((fila) => (
+                        <tr key={fila.id}>
+                            <td>{fila.id}</td>
+                            <td>{fila.Nombre}</td>
+                            <td>{fila.Apellidos}</td>
+                            <td>{fila.Email}</td>
                             <td>
-                                <button class="validate-btn">Validar</button>
-                                <button class="delete-btn">Invalidar</button>
+                                <button class="validate-btn" onClick={() => validarUsuario(fila.id)}>Validar</button>
+                                <button class="delete-btn" onClick={() => invalidarUsuario(fila.id)}>Invalidar</button>
                             </td>
                         </tr>
+                    ))}
                     </tbody>
                 </table>
             </body>
@@ -65,39 +87,19 @@ const UserValidationUI = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Juan</td>
-                            <td>Pérez</td>
-                            <td>juan.perez@example.com</td>
+                    {datosUsuarios.map((fila) => (
+                        <tr key={fila.id}>
+                            <td>{fila.id}</td>
+                            <td>{fila.Nombre}</td>
+                            <td>{fila.Apellidos}</td>
+                            <td>{fila.Email}</td>
                             <td>
                                 <button class="validate-btn">Habilitar</button>
                                 <button class="edit-btn">Modificar</button>
                                 <button class="delete-btn">Eliminar</button>
                             </td>
                         </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>María</td>
-                            <td>López</td>
-                            <td>maria.lopez@example.com</td>
-                            <td>
-                                <button class="validate-btn">Habilitar</button>
-                                <button class="edit-btn">Modificar</button>
-                                <button class="delete-btn">Eliminar</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Carlos</td>
-                            <td>García</td>
-                            <td>carlos.garcia@example.com</td>
-                            <td>
-                                <button class="validate-btn">Habilitar</button>
-                                <button class="edit-btn">Modificar</button>
-                                <button class="delete-btn">Eliminar</button>
-                            </td>
-                        </tr>
+                    ))}
                     </tbody>
                 </table>
             </body>
