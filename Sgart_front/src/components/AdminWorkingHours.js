01b56f4ee2866e28fe18ca60efefcba87b59fcac
Alberto Cazallas Monje <115149185+AlbertoCazallasMonje@users.noreply.github.com>
2024-10-17 12:23:20 +0200

    Mejoras de calidad
    Se han añadido mejoras para que el proceso de creación de un evento y todos sus campos coincidan con los planteados para la base de datos.

--- Sgart_front/src/components/AdminWorkingHours.js
+++ Sgart_front/src/components/AdminWorkingHours.js
@@ -7,25 +7,27 @@ import interactionPlugin from "@fullcalendar/interaction";
 import '../App.css';
 
 const AdminWorkingHours = () => {
-
-    const [selectedDate, setSelectedDate] = useState('');  // Estado para modificar día
-    const [startingHour, setStartingHour] = useState('');  // Estado para modificar el horario de trabajo
-    const [endingHour, setEndingHour] = useState('');      // Estado para modificar el horario de trabajo
+    // Variables de la sección de modificar día laborable
+    const [selectedDate, setSelectedDate] = useState('');
+    const [startingHour, setStartingHour] = useState('');
+    const [endingHour, setEndingHour] = useState('');
     const [eventName, setEventName] = useState('');
-    const [popupSelectedDate, setPopupSelectedDate] = useState('');  // Nuevo estado para la fecha en el pop-up
-    const [eventFrequency, setEventFrequency] = useState('Una vez');  // Estado para la recurrencia
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    // Nuevos estados para las horas del pop-up de añadir evento
+    // Variables de la sección del pop-up de creación de un evento
+    const [popupSelectedDate, setPopupSelectedDate] = useState('');
+    const [eventFrequency, setEventFrequency] = useState('Una vez');
+    const [isAllDay, setIsAllDay] = useState(false);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
     const [popupStartingHour, setPopupStartingHour] = useState('');
     const [popupEndingHour, setPopupEndingHour] = useState('');
 
-    const [isEventDetailPopupOpen, setIsEventDetailPopupOpen] = useState(false); // Pop-up de detalles
-    const [selectedEvent, setSelectedEvent] = useState(null); // Evento seleccionado para mostrar detalles
+    // Variables para el pop-up de detalles de los eventos
+    const [isEventDetailPopupOpen, setIsEventDetailPopupOpen] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
 
     /* Los eventos se cargarán de la base de datos de eventos */
     const [events, setEvents] = useState([
-        { title: 'Evento 1', start: '2024-10-15T10:00:00', end: '2024-10-15T12:00:00', eventFrequency: 'Una vez' },
+        { title: 'Evento 1', start: '2024-10-15T10:00:00', end: '2024-10-15T12:00:00', allDay: false, eventFrequency: 'Una vez' },
         { title: 'Evento 2', start: '2024-10-20', allDay: true, eventFrequency: 'Semanal' },
     ]);
 
@@ -41,38 +43,40 @@ const AdminWorkingHours = () => {
     };
 
     const handleAddTimeClick = () => {
-        setPopupStartingHour('');  // Limpiar los campos de hora del pop-up de añadir evento
+        setPopupStartingHour('');
         setPopupEndingHour('');
         setEventName('');
-        setPopupSelectedDate('');  // Limpiar la fecha del pop-up
-        setEventFrequency('Una vez');  // Restablecer la frecuencia por defecto
-        setIsPopupOpen(true); // Abrir el pop-up para añadir un nuevo evento
+        setPopupSelectedDate('');
+        setEventFrequency('Una vez');
+        setIsAllDay(false);
+        setIsPopupOpen(true);
     };
 
     const handleClosePopup = () => {
-        setIsPopupOpen(false); // Cerrar el pop-up para añadir eventos
+        setIsPopupOpen(false);
     };
 
     const handleSaveEvent = () => {
         const newEvent = {
             title: eventName,
-            start: popupSelectedDate + 'T' + popupStartingHour,
-            end: popupSelectedDate + 'T' + popupEndingHour,
-            eventFrequency,  // Añadimos la frecuencia seleccionada
+            start: popupSelectedDate + (isAllDay ? '' : 'T' + popupStartingHour),
+            end: isAllDay ? null : popupSelectedDate + 'T' + popupEndingHour,
+            allDay: isAllDay,
+            eventFrequency,
         };
         setEvents([...events, newEvent]);
-        setIsPopupOpen(false); // Cierra el pop-up después de guardar
+        setIsPopupOpen(false);
     };
 
     // Maneja el click en un evento del calendario para mostrar el pop-up de detalles
     const handleEventClick = (clickInfo) => {
-        setSelectedEvent(clickInfo.event); // Guarda la información del evento
-        setIsEventDetailPopupOpen(true); // Abre el pop-up de detalles
+        setSelectedEvent(clickInfo.event);
+        setIsEventDetailPopupOpen(true);
     };
 
     const handleCloseEventDetailPopup = () => {
         setIsEventDetailPopupOpen(false);
-        setSelectedEvent(null); // Limpia la información del evento
+        setSelectedEvent(null);
     };
 
     return (
@@ -83,7 +87,7 @@ const AdminWorkingHours = () => {
                     <label>Fecha:</label>
                     <input
                         type="date"
-                        value={selectedDate}  // Campo de fecha independiente para modificar el día
+                        value={selectedDate}
                         onChange={(e) => setSelectedDate(e.target.value)}
                     />
                 </div>
@@ -92,7 +96,7 @@ const AdminWorkingHours = () => {
                     <input
                         type="text"
                         placeholder="Hora de inicio"
-                        value={startingHour}  // Este campo ahora es independiente
+                        value={startingHour}
                         onChange={(e) => setStartingHour(e.target.value)}
                     />
                 </div>
@@ -101,7 +105,7 @@ const AdminWorkingHours = () => {
                     <input
                         type="text"
                         placeholder="Hora de fin"
-                        value={endingHour}  // Este campo ahora es independiente
+                        value={endingHour}
                         onChange={(e) => setEndingHour(e.target.value)}
                     />
                 </div>
@@ -123,7 +127,7 @@ const AdminWorkingHours = () => {
                         initialView="dayGridMonth"
                         events={events}
                         dateClick={handleDateClick}
-                        eventClick={handleEventClick} // Agrega el handler de click en evento
+                        eventClick={handleEventClick}
                         selectable={true}
                         businessHours={{
                             daysOfWeek: [1, 2, 3, 4, 5],
@@ -152,28 +156,40 @@ const AdminWorkingHours = () => {
                             <label>Fecha:</label>
                             <input
                                 type="date"
-                                value={popupSelectedDate}  // Campo de fecha independiente para el pop-up
+                                value={popupSelectedDate}
                                 onChange={(e) => setPopupSelectedDate(e.target.value)}
                             />
                         </div>
                         <div className="AdminCalendar-input-group">
-                            <label>Hora de inicio:</label>
+                            <label>¿Es un evento de todo el día?</label>
                             <input
-                                type="text"
-                                placeholder="Hora de inicio"
-                                value={popupStartingHour}  // Campo independiente para el pop-up
-                                onChange={(e) => setPopupStartingHour(e.target.value)}
+                                type="checkbox"
+                                checked={isAllDay}
+                                onChange={(e) => setIsAllDay(e.target.checked)}
                             />
                         </div>
-                        <div className="AdminCalendar-input-group">
-                            <label>Hora de fin:</label>
-                            <input
-                                type="text"
-                                placeholder="Hora de fin"
-                                value={popupEndingHour}  // Campo independiente para el pop-up
-                                onChange={(e) => setPopupEndingHour(e.target.value)}
-                            />
-                        </div>
+                        {!isAllDay && (
+                            <>
+                                <div className="AdminCalendar-input-group">
+                                    <label>Hora de inicio:</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Hora de inicio"
+                                        value={popupStartingHour}
+                                        onChange={(e) => setPopupStartingHour(e.target.value)}
+                                    />
+                                </div>
+                                <div className="AdminCalendar-input-group">
+                                    <label>Hora de fin:</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Hora de fin"
+                                        value={popupEndingHour}
+                                        onChange={(e) => setPopupEndingHour(e.target.value)}
+                                    />
+                                </div>
+                            </>
+                        )}
                         <div className="AdminCalendar-input-group">
                             <label>Frecuencia del Evento:</label>
                             <select
@@ -208,17 +224,29 @@ const AdminWorkingHours = () => {
                             <label>Fecha de Inicio:</label>
                             <p>{selectedEvent.start.toLocaleString()}</p>
                         </div>
-                        <div className="AdminCalendar-input-group">
-                            <label>Fecha de Fin:</label>
-                            {selectedEvent.end ? (
-                                <p>{selectedEvent.end.toLocaleString()}</p>
-                            ) : (
-                                <p>No definida</p>
-                            )}
-                        </div>
+                        {selectedEvent.allDay ? (
+                            <div className="AdminCalendar-input-group">
+                                <label>Este evento es de todo el día</label>
+                            </div>
+                        ) : (
+                            <>
+                                <div className="AdminCalendar-input-group">
+                                    <label>Hora de inicio:</label>
+                                    <p>{selectedEvent.start.toLocaleTimeString()}</p>
+                                </div>
+                                <div className="AdminCalendar-input-group">
+                                    <label>Hora de fin:</label>
+                                    {selectedEvent.end ? (
+                                        <p>{selectedEvent.end.toLocaleTimeString()}</p>
+                                    ) : (
+                                        <p>No definida</p>
+                                    )}
+                                </div>
+                            </>
+                        )}
                         <div className="AdminCalendar-input-group">
                             <label>Frecuencia del Evento:</label>
-                            <p>{selectedEvent.extendedProps.eventFrequency}</p> {/* Mostrar la frecuencia */}
+                            <p>{selectedEvent.extendedProps.eventFrequency}</p>
                         </div>
                         <button className="close-button" onClick={handleCloseEventDetailPopup}>Cerrar</button>
                     </div>
