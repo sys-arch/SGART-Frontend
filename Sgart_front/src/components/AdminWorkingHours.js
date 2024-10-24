489ea8bcc3cfd6b8c7211d9b045113874966ff99
Alberto Cazallas Monje <115149185+AlbertoCazallasMonje@users.noreply.github.com>
2024-10-24 11:10:57 +0200

    Cambios de funcionalidad
    Se han añadido las funcionalidades para poder cargar los datos de un nuevo evento a la base de datos y cargar los eventos desde la base de datos al calendario.

--- Sgart_front/src/components/AdminWorkingHours.js
+++ Sgart_front/src/components/AdminWorkingHours.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -9,7 +9,9 @@ const AdminWorkingHours = () => {
     // Variables para modificar día laborable
     const [selectedDate, setSelectedDate] = useState('');
     const [startingHour, setStartingHour] = useState('');
+    const [startingMinutes, setStartingMinutes] = useState(''); // Nuevos minutos para el inicio
     const [endingHour, setEndingHour] = useState('');
+    const [endingMinutes, setEndingMinutes] = useState(''); // Nuevos minutos para el fin
     const [eventName, setEventName] = useState('');
 
     // Variables del pop-up de creación de eventos
@@ -18,7 +20,9 @@ const AdminWorkingHours = () => {
     const [isAllDay, setIsAllDay] = useState(false);
     const [isPopupOpen, setIsPopupOpen] = useState(false);
     const [popupStartingHour, setPopupStartingHour] = useState('');
+    const [popupStartingMinutes, setPopupStartingMinutes] = useState(''); // Nuevos minutos para el pop-up
     const [popupEndingHour, setPopupEndingHour] = useState('');
+    const [popupEndingMinutes, setPopupEndingMinutes] = useState(''); // Nuevos minutos para el pop-up
 
     // Nueva variable para abrir el pop-up de personalización
     const [isCustomPopupOpen, setIsCustomPopupOpen] = useState(false);
@@ -29,13 +33,40 @@ const AdminWorkingHours = () => {
     const [isEventDetailPopupOpen, setIsEventDetailPopupOpen] = useState(false);
     const [selectedEvent, setSelectedEvent] = useState(null);
 
-    // Eventos de ejemplo
-    const [events, setEvents] = useState([
-        { title: 'Evento 1', start: '2024-10-15T10:00:00', end: '2024-10-15T12:00:00', allDay: false, eventFrequency: 'Una vez' },
-        { title: 'Evento 2', start: '2024-10-20', allDay: true, eventFrequency: 'Semanal' },
-    ]);
+    //Cargar los eventos de la base de datos
+    const loadEvents = useCallback(async () => {
+        try {
+            const response = await fetch('http://localhost:9000/administrador/eventos/loadEvents');
+            if (!response.ok) {
+                throw new Error('Error al cargar los eventos');
+            }
+    
+            const backendEvents = await response.json();
+            const transformedEvents = transformEvents(backendEvents);
+            setEvents(transformedEvents);
+        } catch (error) {
+            console.error("Error al cargar los eventos: ", error);
+        }
+    }, []);
 
-    /* Horarios de trabajo por defecto */
+    const transformEvents = (backendEvents) => {
+        return backendEvents.map(event => ({
+            title: event.event_title,
+            start: `${event.event_start_date}T${event.event_time_start}`,
+            end: `${event.event_start_date}T${event.event_time_end}`,
+            allDay: event.event_all_day === 1, // convertir el valor del backend en un booleano
+            eventFrequency: event.event_frequency
+        }));
+    };
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        // Hacer la solicitud para obtener los eventos guardados en la base de datos
+        loadEvents();
+    }, [loadEvents]);
+
+    /* Horarios de trabajo por defecto - Debe cargarse de la base de datos de Working Hours */
     const defaultStartTime = '08:00';
     const defaultEndTime = '15:00';
 
@@ -43,14 +74,18 @@ const AdminWorkingHours = () => {
     const handleDateClick = (arg) => {
         const clickedDate = arg.dateStr;
         setSelectedDate(clickedDate);
-        setStartingHour(defaultStartTime);
-        setEndingHour(defaultEndTime);
+        setStartingHour(defaultStartTime.split(":")[0]);
+        setStartingMinutes(defaultStartTime.split(":")[1]);
+        setEndingHour(defaultEndTime.split(":")[0]);
+        setEndingMinutes(defaultEndTime.split(":")[1]);
     };
 
     // Añadir un nuevo evento
     const handleAddTimeClick = () => {
         setPopupStartingHour('');
+        setPopupStartingMinutes('');
         setPopupEndingHour('');
+        setPopupEndingMinutes('');
         setEventName('');
         setPopupSelectedDate('');
         setEventFrequency('Una vez');
@@ -69,19 +104,50 @@ const AdminWorkingHours = () => {
     };
 
     // Guardar el nuevo evento
-    const handleSaveEvent = () => {
+    // Guardar el nuevo evento
+    const handleSaveEvent = async () => {
+        // Combina las horas y minutos en el formato HH:mm para la base de datos SQL
+        const startingTime = `${popupStartingHour.padStart(2, '0')}:${popupStartingMinutes.padStart(2, '0')}:00`;
+        const endingTime = `${popupEndingHour.padStart(2, '0')}:${popupEndingMinutes.padStart(2, '0')}:00`;
+
+        // Crear el objeto del evento
         const newEvent = {
-            title: eventName,
-            start: popupSelectedDate + (isAllDay ? '' : 'T' + popupStartingHour),
-            end: isAllDay ? null : popupSelectedDate + 'T' + popupEndingHour,
-            allDay: isAllDay,
-            eventFrequency: eventFrequency === 'Personalizado' 
-                ? `Personalizado - ${customFrequency}, ${repeatCount} veces` 
-                : eventFrequency,
+            event_title: eventName,
+            event_start_date: popupSelectedDate,  // Solo la fecha, sin la hora
+            event_all_day: isAllDay ? 1 : 0,  // Convertimos el booleano a 0 o 1
+            event_time_start: isAllDay ? '00:00:00' : startingTime,
+            event_time_end: isAllDay ? '23:59:59' : endingTime,
+            event_frequency: eventFrequency === 'Personalizado' ? customFrequency : eventFrequency, // Frecuencia
+            event_repetitions_count: eventFrequency === 'Personalizado' ? parseInt(repeatCount, 10) : -1, // Repeticiones o null
         };
-        setEvents([...events, newEvent]);
-        setIsPopupOpen(false);
+
+        // Log para comprobar lo que estás enviando
+        console.log("Evento que se enviará al backend:", newEvent);
+
+        try {
+            // Envía el evento al backend usando fetch
+            const response = await fetch('http://localhost:9000/administrador/eventos/saveEvent', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newEvent),
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al guardar el evento');
+            }
+
+            const savedEvent = await response.json();
+            setEvents([...events, savedEvent]);  // Actualiza el estado de los eventos
+            await loadEvents();
+            setIsPopupOpen(false);  // Cierra el pop-up
+            alert("[!] Se ha guardado el evento de manera exitosa.")
+        } catch (error) {
+            console.error('Error al guardar el evento:', error);
+        }
     };
+
 
     // Manejar la selección de repetición personalizada
     const handleCustomFrequency = () => {
@@ -108,13 +174,13 @@ const AdminWorkingHours = () => {
         <div className='AdminCalendarapp-container'>
             <div class="admin-buttons">
                 <button class="admin-btn">
-                    <img src={require('../media/user_management_btn.png')} width={60}/>
+                    <img src={require('../media/user_management_btn.png')} width={60} />
                 </button>
                 <button class="admin-btn">
-                    <img src={require('../media/admin_management_btn.png')} width={60}/>
+                    <img src={require('../media/admin_management_btn.png')} width={60} />
                 </button>
                 <button class="admin-btn">
-                    <img src={require('../media/calendar_management_btn.png')} width={60}/>
+                    <img src={require('../media/calendar_management_btn.png')} width={60} />
                 </button>
             </div>
             <div className="AdminCalendar-left-panel">
@@ -129,21 +195,47 @@ const AdminWorkingHours = () => {
                 </div>
                 <div className="AdminCalendar-input-group">
                     <label>Hora de inicio:</label>
-                    <input
-                        type="text"
-                        placeholder="Hora de inicio"
-                        value={startingHour}
-                        onChange={(e) => setStartingHour(e.target.value)}
-                    />
+                    <div>
+                        <input
+                            type="number"
+                            placeholder="HH"
+                            value={startingHour}
+                            onChange={(e) => setStartingHour(e.target.value)}
+                            min="0"
+                            max="23"
+                        />
+                        :
+                        <input
+                            type="number"
+                            placeholder="MM"
+                            value={startingMinutes}
+                            onChange={(e) => setStartingMinutes(e.target.value)}
+                            min="0"
+                            max="59"
+                        />
+                    </div>
                 </div>
                 <div className="AdminCalendar-input-group">
                     <label>Hora de fin:</label>
-                    <input
-                        type="text"
-                        placeholder="Hora de fin"
-                        value={endingHour}
-                        onChange={(e) => setEndingHour(e.target.value)}
-                    />
+                    <div>
+                        <input
+                            type="number"
+                            placeholder="HH"
+                            value={endingHour}
+                            onChange={(e) => setEndingHour(e.target.value)}
+                            min="0"
+                            max="23"
+                        />
+                        :
+                        <input
+                            type="number"
+                            placeholder="MM"
+                            value={endingMinutes}
+                            onChange={(e) => setEndingMinutes(e.target.value)}
+                            min="0"
+                            max="59"
+                        />
+                    </div>
                 </div>
                 <div className="AdminCalendar-button-group">
                     <button>Editar día</button>
@@ -208,21 +300,47 @@ const AdminWorkingHours = () => {
                             <>
                                 <div className="AdminCalendar-input-group">
                                     <label>Hora de inicio:</label>
-                                    <input
-                                        type="text"
-                                        placeholder="Hora de inicio"
-                                        value={popupStartingHour}
-                                        onChange={(e) => setPopupStartingHour(e.target.value)}
-                                    />
+                                    <div>
+                                        <input
+                                            type="number"
+                                            placeholder="HH"
+                                            value={popupStartingHour}
+                                            onChange={(e) => setPopupStartingHour(e.target.value)}
+                                            min="0"
+                                            max="23"
+                                        />
+                                        :
+                                        <input
+                                            type="number"
+                                            placeholder="MM"
+                                            value={popupStartingMinutes}
+                                            onChange={(e) => setPopupStartingMinutes(e.target.value)}
+                                            min="0"
+                                            max="59"
+                                        />
+                                    </div>
                                 </div>
                                 <div className="AdminCalendar-input-group">
                                     <label>Hora de fin:</label>
-                                    <input
-                                        type="text"
-                                        placeholder="Hora de fin"
-                                        value={popupEndingHour}
-                                        onChange={(e) => setPopupEndingHour(e.target.value)}
-                                    />
+                                    <div>
+                                        <input
+                                            type="number"
+                                            placeholder="HH"
+                                            value={popupEndingHour}
+                                            onChange={(e) => setPopupEndingHour(e.target.value)}
+                                            min="0"
+                                            max="23"
+                                        />
+                                        :
+                                        <input
+                                            type="number"
+                                            placeholder="MM"
+                                            value={popupEndingMinutes}
+                                            onChange={(e) => setPopupEndingMinutes(e.target.value)}
+                                            min="0"
+                                            max="59"
+                                        />
+                                    </div>
                                 </div>
                             </>
                         )}
@@ -240,7 +358,6 @@ const AdminWorkingHours = () => {
                                 <option value="Semanal">Semanal</option>
                                 <option value="Mensual">Mensual</option>
                                 <option value="Anual">Anual</option>
-                                <option value="Siempre">Siempre</option>
                                 <option value="Personalizado">Personalizado...</option>
                             </select>
                         </div>
