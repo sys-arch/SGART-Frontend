cc76c629baf532e1cc44f844eb3ed88827325c9d
EnriqueReyesGonzalez <115165698+EnriqueReyesGonzalez@users.noreply.github.com>
2024-10-30 18:15:47 +0100

    Front autenticator y llevar datos desde el registro

--- Sgart_front/src/components/GoogleAuth.js
+++ Sgart_front/src/components/GoogleAuth.js
@@ -1,19 +1,64 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../App.css';
 
 const GoogleAuth = () => {
-
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { usuario } = location.state;
     const [inputCode, setInputCode] = useState('');
-    //const [qrValue, setQrValue] = useState('');
+    const [message, setMessage] = useState('');
+    const [qrCode, setQrCode] = useState('');
 
-    //useEffect(() => {
-    // Generar el código QR automáticamente al cargar la página
-    //setQrValue('https://www.tu-sitio.com');
-    //}, []);
+    useEffect(() => {
+        const fetchQRCode = async () => {
+            try {
+                const response = await fetch(`http://localhost:9000/auth/generate-qr?email=${usuario.email}`);
+                if (!response.ok) {
+                    throw new Error('Error al generar el código QR');
+                }
+                const data = await response.json();
+                setQrCode(data.qrCode);
+            } catch (error) {
+                setMessage('Error al generar el código QR');
+                console.error('Error al generar el código QR:', error);
+            }
+        };
+
+        fetchQRCode();
+    }, [usuario.email]);
 
     const handleInputChange = (event) => {
-        setInputCode(event.target.value); // Actualiza el código ingresado
+        setInputCode(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            const response = await fetch('http://localhost:9000/auth/validate-totp', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ mail: usuario.email, code: inputCode }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Código de autenticación incorrecto');
+            }
+
+            const data = await response.json();
+            if (data.status === 'valid') {
+                setMessage("Autenticación exitosa. Redirigiendo...");
+                navigate('/user-options');
+            } else {
+                setMessage("Código inválido. Por favor, intenta nuevamente.");
+            }
+        } catch (error) {
+            setMessage("Error al validar el código.");
+            console.error("Error en la autenticación:", error);
+        }
     };
 
     return (
@@ -22,11 +67,15 @@ const GoogleAuth = () => {
                 <h2>Bienvenido</h2>
                 <p>Por favor escanea el código QR con tu dispositivo móvil para configurar Google Authenticator:</p>
                 <div className="qr-container">
-                    <div className='qr-placeholder'>
-                        <p>Código QR no disponible</p>
-                    </div>
+                    {qrCode ? (
+                        <img src={`data:image/png;base64,${qrCode}`} alt="Código QR" />
+                    ) : (
+                        <div className='qr-placeholder'>
+                            <p>Código QR no disponible</p>
+                        </div>
+                    )}
                 </div>
-                <br></br>
+                <br />
                 <h3>Introduce tu código de Google Authenticator</h3>
                 <input
                     type="text"
@@ -36,11 +85,12 @@ const GoogleAuth = () => {
                     className="code-input"
                     placeholder="Introduce el código"
                 />
-                <br></br>
-                <br></br>
+                <br />
+                <br />
                 <div>
-                    <button type='sumbit' className='login-btn'>Ir al Inicio de Sesión</button>
+                    <button onClick={handleSubmit} className='login-btn'>Verificar</button>
                 </div>
+                {message && <p>{message}</p>}
             </div>
         </div>
     );
