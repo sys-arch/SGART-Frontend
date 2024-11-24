bd830411cf0218862b713bc3c8b704a5a86c1531
EnriqueReyesGonzalez <115165698+EnriqueReyesGonzalez@users.noreply.github.com>
2024-11-24 02:40:39 +0100

    Separacion de errores 401 y 403
    403 para mostrar los intnentos de inicio
    401 para mostrar algun error con los datos introducidos

--- Sgart_front/src/components/LoginForm.js
+++ Sgart_front/src/components/LoginForm.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import RegisterForm from './RegisterForm';
 import RecuperarPwdForm from './RecuperarPwdForm';
 import { useNavigate } from 'react-router-dom';
-import LoadingSpinner from './LoadingSpinner';
 
 const LoginForm = () => {
     const navigate = useNavigate();
@@ -17,7 +16,6 @@ const LoginForm = () => {
     const [error, setError] = useState(false);
     const [errorEmail, setErrorEmail] = useState('');
     const [errorPassword, setErrorPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
 
     // Cambios en los campos del formulario
     const handleChange = (event) => {
@@ -34,34 +32,38 @@ const LoginForm = () => {
         }
     };
 
+    const toogleError = useCallback(() => {
+        setError(true);
+    });
+
     const handleLogin = () => {
-        let errorBool = false;
+        setError(false);
         setErrorEmail('');
         setErrorPassword('');
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             setErrorEmail('El formato del correo electrónico no es válido.');
-            errorBool = true;
+            toogleError();
         }
 
         if (email === '') {
             setErrorEmail('Campo vacío');
-            errorBool = true;
+            toogleError();
         }
         if (contrasena === '') {
             setErrorPassword('Campo vacío');
-            errorBool = true;
+            toogleError();
         }
 
-        if (errorBool) {
+        if (error) {
             return;
         } else {
             const user = {
                 email: email,
                 password: contrasena,
             };
-            setIsLoading(true);
+
             fetch('http://localhost:9000/users/login', {
                 method: 'POST',
                 headers: {
@@ -72,32 +74,29 @@ const LoginForm = () => {
                 .then((response) => {
                     if (!response.ok) {
                         return response.json().then((data) => {
-                            throw new Error(data.message || 'Error al iniciar sesión. Correo y/o contraseña incorrectos');
+                            if (response.status === 403) 
+                                throw new Error('Los inicios de sesión están bloqueados temporalmente.');
+                            
+                            else if (response.status === 401) 
+                                throw new Error(data.message || 'Error al iniciar sesión. Correo y/o contraseña incorrectos');
                         });
                     }
                     return response.json();
                 })
                 .then((data) => {
-                    if (data.user && data.user.blocked) {
+                    if (data.user?.blocked) {
                         alert('Tu cuenta está bloqueada. Por favor, contacta al soporte.');
                         return;
                     }
 
+                    // Redirección después del login exitoso
                     alert('Login exitoso. Pasando a la autentificación con doble factor...');
                     navigate('/google-auth-login', { state: { data: data, email: email } });
                 })
                 .catch((error) => {
                     alert(error.message);
-                })
-                .finally(() => {
-                    setIsLoading(false);
                 });
         }
-    };
-
-    const handle2FAComplete = () => {
-        // Redirige a una ruta específica después del 2FA
-        navigate('/user-calendar');
     };
 
     const togglePasswordVisibility = () => {
@@ -108,70 +107,42 @@ const LoginForm = () => {
         setIsRegistering(true);
     };
 
-    if (isRegistering) {
-        return <RegisterForm />;
-    }
-
     const handleToggleForm_2 = () => {
         setIsNoPwd(true);
     };
+
+    if (isRegistering) {
+        return <RegisterForm />;
+    }
 
     if (isNoPwd) {
         return <RecuperarPwdForm />;
     }
 
     return (
-        <>
-            {isLoading ? (
-                <LoadingSpinner />
-            ) : (
-                <div className="login-container">
-                    <div className="login-box">
-                        <h2 className="title">Iniciar Sesión</h2>
-                        <div className={errorEmail === '' ? "input-group" : "input-group-error"}>
-                            <input
-                                type="text"
-                                id="email"
-                                name="email"
-                                value={email}
-                                onChange={handleChange}
-                                required
-                            />
-                            <label htmlFor="email">Usuario</label>
-                        </div>
-                        <label className="error">{errorEmail}</label>
-                        <div className={errorPassword === '' ? "input-group" : "input-group-error"}>
-                            <input
-                                type={showPassword ? "text" : "password"}
-                                id="contrasena"
-                                name="contrasena"
-                                value={contrasena}
-                                onChange={handleChange}
-                                required
-                            />
-                            <label htmlFor="contrasena">Contraseña</label>
-                            <button
-                                type="button"
-                                onClick={togglePasswordVisibility}
-                                className="password-toggle-btn"
-                            >
-                                <img
-                                    src={require(showPassword ? '../media/password_off.png' : '../media/password_on.png')}
-                                    alt="Mostrar Contraseña"
-                                />
-                            </button>
-                        </div>
-                        <label className="error">{errorPassword}</label>
-                        <button onClick={handleLogin} className="login-btn">Entrar</button>
-                        <div className="login-options">
-                            <a href="#" onClick={handleToggleForm_2}>¿Olvidaste tu contraseña?</a>
-                            <a href="#" onClick={handleToggleForm}>Regístrate</a>
-                        </div>
-                    </div>
+        <div className="login-container">
+            <div className="login-box">
+                <h2 className="login-title">Iniciar Sesión</h2>
+                <div className={errorEmail === '' ? "input-group" : "input-group-error"}>
+                    <input type="text" id="email" name="email" value={email} onChange={handleChange} required />
+                    <label htmlFor="email">Usuario</label>
                 </div>
-
-            )}
-        </>
+                <label className="error">{errorEmail}</label>
+                <div className={errorPassword === '' ? "input-group" : "input-group-error"}>
+                    <input type={showPassword ? "text" : "password"} id="contrasena" name="contrasena" value={contrasena} onChange={handleChange} required />
+                    <label htmlFor="contrasena">Contraseña</label>
+                    <button type="button" onClick={togglePasswordVisibility} className="password-toggle-btn">
+                        <img src={require(showPassword ? '../media/password_off.png' : '../media/password_on.png')} alt='Mostrar Contraseña' />
+                    </button>
+                </div>
+                <label className="error">{errorPassword}</label>
+                <button onClick={handleLogin} className="login-btn">Entrar</button>
+                <div className="login-options">
+                    <a href="#" onClick={handleToggleForm_2}>¿Olvidaste tu contraseña?</a>
+                    <a href="#" onClick={handleToggleForm}>Regístrate</a>
+                </div>
+            </div>
+        </div>
     );
 };
 
